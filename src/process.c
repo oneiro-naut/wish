8de310a18977c53da4c1b74eb9c086df1a495efa
Ayush <ayush@disroot.org>
2020-01-21 12:55:25 +0530

    fixed minor bug in process.c

--- src/process.c
+++ src/process.c
@@ -8,7 +8,7 @@ int waitfor(pid_t pid)
     int EXIT_STAT;
     do{
         waitpid(pid,&EXIT_STAT,WUNTRACED);
-    }while(!WIFEXITED(EXIT_STAT)&&WIFSIGNALED(EXIT_STAT));
+    }while(!WIFEXITED(EXIT_STAT)&&!WIFSIGNALED(EXIT_STAT));
 
     return EXIT_STAT;
 }
